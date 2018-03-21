7b7586ae79d87d384e581fb85f42686f079b2218
Kyle Beckles <kyle@arweave.org>
2018-03-21 09:23:13 +0100

    - Updated composer.json to switch over to packgist for arweave sdk.

--- provision.sh
+++ provision.sh
@@ -1,6 +1,9 @@
+echo '-- Installing apache and php mods --';
 apt-get update
-apt-get install -y apache2 composer php7.0-zip php7.0-curl libapache2-mod-php7.0 php7.0-gmp
+apt-get install -y zip unzip apache2 composer libapache2-mod-php7.0 php7.0-zip php7.0-curl php7.0-mbstring php7.0-gmp
+echo '-- Successfully installed apache and php mods --';
 
+echo '-- Creating apache vhost --';
 VHOST=$(cat <<EOF
 <VirtualHost *:80>
   DocumentRoot "/var/www/html/app/public"
@@ -12,10 +15,13 @@ EOF
 )
 
 echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+echo '-- Successfully created apache vhost --';
 
+echo '-- Enabling Apache mods  --';
 # Enable mod_rewrite
 a2enmod rewrite
 a2enmod php7.0
 
 # Restart apache
 service apache2 restart
+echo '-- Successfully enabled Apache mods --';
